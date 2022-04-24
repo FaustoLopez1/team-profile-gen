7820fefe232ae794694f55d8528cd729c0486fec
Fausto Lopez <lopezfausto123@yahoo.com>
2022-04-24 14:00:07 -0700

    starter code

---
+++ Develop/lib/Employee.js
@@ -0,0 +1,38 @@
+//name
+class Employee {
+    constructor(id, email, name) {
+        //id
+      this.id = id;
+      //email
+      this.email = email;
+      //name
+      this.name = name;
+    }
+    
+    //getName()
+    getName() {
+        return this.name;
+    }
+
+    //getRole()—returns 'Employee'
+    getRole() {
+        return 'Employee';
+    }
+
+    //getemail
+    getEmail() {
+        return this.email;
+    }
+
+    //get id
+    getId() {
+        return this.id;
+    }
+
+  }
+  module.exports = Employee;
+  
+
+
+
+
