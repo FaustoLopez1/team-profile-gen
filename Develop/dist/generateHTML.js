67193b214b7f2994d04a361460974a66db361089
Fausto Lopez <lopezfausto123@yahoo.com>
2022-04-25 18:49:09 -0700

    Finished without read.me

--- Develop/dist/generateHTML.js
+++ Develop/dist/generateHTML.js
@@ -1,16 +1,21 @@
 const generateHTML = (employeeList = []) =>
+
   `<!DOCTYPE html>
 <html lang="en">
 <head>
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
-  <title>Document</title>
-</head>
-<body>
-  <div class="jumbotron jumbotron-fluid">
+  <title>Team Profile Generator</title>
+  </head>
+  <body>
+  <div class = " text-center p-3 bg-primary text-white">
+    <h1>MY TEAM</h1>
+  </div>  
   <div class="container">
+  <div class="row">
    ${insertlist(employeeList)}
+  </div>
   </div>
 </div>
 </body>
@@ -22,34 +27,42 @@ const insertlist = (employeeList) => {
   let employeeHtml = "";
   employeeList.forEach(person => {
     employeeHtml = employeeHtml.concat(
-      `<div class = person-container>
-      
-      <div>${getPerson(person)}</div>
-      </div>`
+      `<div class="col-sm-3 m-4 p-3 ">
+      <div class="card m-1 shadow" style="width: 18rem">
+      <div class='card-header bg-info'>
+        <h3 class="card-title">${person.getName()}</h3>
+        <h5 class="card-text"><i class="fa fa-laptop"></i> ${person.getRole()}</h6>
+      </div>
+      <div class="card-body bg-secondary">
+        <ul class="list-group list-group-flush">
+          <li class="list-group-item">ID: ${person.getId()}</li>
+          <li class="list-group-item">Email: ${createAncor(person.getEmail())}</li>
+          <li class="list-group-item">${getPerson(person)}</li>
+        </ul>
+      </div>
+    </div>
+    </div>`
     )
   })
   return employeeHtml;
 }
-
+// creates the links
 const createAncor = (title) => {
   return `<a href="">${title}</a>`
 }
 
 const getPerson = (person) => {
   if (person.getRole() === "Manager") {
-    return "Office number: " + person.office;
+    console.log(person)
+    return "Office number: " + person.officeNumber;
   }
   else if (person.getRole() === "Engineer") {
     return "Github: " + createAncor(person.github);
   }
-  else { 
+  else {
     return "School: " + person.school;
   }
 }
 
 
-
-fs.writeFile('README.md', mdPageContent, (err) =>
-                err ? console.log(err) : console.log('Successfully created Read.me!')
-            );
 module.exports = generateHTML;
--- index.js
+++ index.js
@@ -8,40 +8,63 @@ const generateHTML = require('./Develop/dist/generateHTML');
 const employeeList = []
 
 
-  const managerinput = () => {
-    inquirer
-      .prompt([
-        {
-          type: 'input',
-          name: 'name',
-          message: 'What is your Team Managers name?',
-        },
-        {
-          type: 'input',
-          name: 'id',
-          message: 'What is the managers employee id?',
-        },
-        {
-          type: 'input',
-          name: 'email',
-          message: 'What is the managers email address?',
-        },
-        {
-          type: 'input',
-          name: 'office',
-          message: 'What is the managers office number?',
-        },
-      ])
-      .then(answers => {
-        const manager = new Manager(answers.name, answers.id, answers.email, answers.office);
-        employeeList.push(manager)
-        engineerinput();
-      });
-  }
+const managerInput = () => {
+  inquirer
+    .prompt([
+      {
+        type: 'input',
+        name: 'name',
+        message: 'What is your Team Managers name?',
+      },
+      {
+        type: 'input',
+        name: 'id',
+        message: 'What is the managers employee id?',
+      },
+      {
+        type: 'input',
+        name: 'email',
+        message: 'What is the managers email address?',
+      },
+      {
+        type: 'input',
+        name: 'office',
+        message: 'What is the managers office number?',
+      },
+    ])
+    .then(answers => {
+      const manager = new Manager(answers.name, answers.id, answers.email, answers.office);
+      employeeList.push(manager)
+      addEmployee();
+    });
+}
 
+const addEmployee = () => {
+  inquirer
+    .prompt([
+      {
+        type: 'list',
+        name: 'name',
+        message: 'Please select member role:',
+        choices: ['Engineer', 'Intern', 'No more Employees to add'],
+      },
+    ])
+    .then(answers => {
+      console.log(answers)
+      if (answers.name === 'Engineer') {
+        engineerInput();
+      } else if (answers.name === 'Intern') {
+        internInput();
+      } else {
+        const html = generateHTML(employeeList);
+        fs.writeFile('index.html', html, (err) =>
+        err ? console.log(err) : console.log('Successfully created index.html!')
+    );
+      }
 
+    });
 
-  const engineerinput = () => {
+  const engineerInput = () => {
     inquirer
       .prompt([
         {
@@ -65,43 +88,44 @@ const employeeList = []
           message: 'What is the engineer github username?',
         },
       ])
-        .then(answers => {
+      .then(answers => {
         const engineer = new Engineer(answers.name, answers.id, answers.email, answers.github);
-        interninput();
+        employeeList.push(engineer)
+        addEmployee();
       });
 
+  }
+
+  const internInput = () => {
+    inquirer
+      .prompt([
+        {
+          type: 'input',
+          name: 'name',
+          message: 'What is your Team intern name?',
+        },
+        {
+          type: 'input',
+          name: 'id',
+          message: 'What is the intern employee id?',
+        },
+        {
+          type: 'input',
+          name: 'email',
+          message: 'What is the intern email address?',
+        },
+        {
+          type: 'input',
+          name: 'school',
+          message: 'What is the interns school name?',
+        },
+      ])
+      .then(answers => {
+        const intern = new Intern(answers.name, answers.id, answers.email, answers.school);
+        employeeList.push(intern)
+        addEmployee();
+      });
+  }
 }
 
-
-
-
-const interninput = () => {
-  inquirer
-    .prompt([
-      {
-        type: 'input',
-        name: 'name',
-        message: 'What is your Team intern name?',
-      },
-      {
-        type: 'input',
-        name: 'id',
-        message: 'What is the intern employee id?',
-      },
-      {
-        type: 'input',
-        name: 'email',
-        message: 'What is the intern email address?',
-      },
-      {
-        type: 'input',
-        name: 'school',
-        message: 'What is the interns school name?',
-      },
-    ])
-    .then(answers => {
-      const intern = new Intern(answers.name, answers.id, answers.email, answers.school);
-      generateHTML(employeeList);
-    });
-}
-managerinput();
+managerInput();
